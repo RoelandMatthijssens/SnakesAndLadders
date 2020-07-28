bb89f4ffa3598783f2c1b3f01fa2df2d8842c266
enermis <fulgens.ailurus@gmail.com>
2020-07-29 00:56:35 +0200

    Add actual snake and ladder data

--- bundle.js
+++ bundle.js
@@ -159,16 +159,41 @@ const renderGame = require('../app/render')
 
 const game = new Game(10, 10, "2d6")
 const board = game.board
-board.addWarp(3, 37)
-board.addWarp(64, 45)
-//game.addPlayer(new Player("Enermis"))
-game.addPlayer(new Player("Fulgens"))
+const snakes = [
+    [32, 10],
+    [34, 6],
+    [48, 27],
+    [63, 18],
+    [88, 24],
+    [95, 56],
+    [97, 78],
+]
+const ladders = [
+    [8, 20],
+    [4, 14],
+    [2, 38],
+    [21, 42],
+    [28, 76],
+    [50, 67],
+    [71, 92],
+    [80, 98],
+]
 function setup() {
     createCanvas(800, 800);
     background(0)
     frameRate(5)
     rectMode(CENTER)
     textAlign(CENTER, CENTER)
+
+    game.addPlayer(new Player("Enermis"))
+    game.addPlayer(new Player("Fulgens"))
+    for (const [from, to] of ladders) {
+        board.addWarp(from, to)
+    }
+    for (const [from, to] of snakes) {
+        board.addWarp(from, to)
+    }
+
     renderGame(game, canvas)
 }
 
